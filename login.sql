9789474a24e80f0ca2c867df01dedb6d8eac2bbc
Kimiyuki Yamauchi <yamauchi@std.it-college.ac.jp>
2014-09-30 21:41:04 +0900

    login.sqlを追加

---
+++ login.sql
@@ -0,0 +1,21 @@
+DEFINE _EDITOR="/usr/bin/vim"
+set null (null)
+set  pages 30
+col first_name for a15
+col last_name for a15
+col name for a15
+col empno for 9999
+col ename for a15
+col yomi for a15
+col job for a15
+col deptno for 999
+col job_id for a10
+col pname for a20
+col department_name for a20
+col city for a20
+col region_name for a30
+col country_name for a30
+col job_title for a20
+col email for a10
+col region for a4
+col dname for a20
