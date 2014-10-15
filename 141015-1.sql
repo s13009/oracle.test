0fc8344b2069121fd518fb7a608721a927f70868
s13009 <s13009@std.it-college.ac.jp>
2014-10-15 15:39:44 +0900

    10/15課題

--- 141015-1.sql
+++ 141015-1.sql
@@ -1,4 +1,7 @@
-select ename, hiredate
+alter session set nls_date_format = 'DD-MON-RR';
+alter session set nls_date_language = 'AMERICAN';
+
+select ename, hiredate, deptno
 from employees
 where deptno = 10
 /
--- 141015-2.sql
+++ 141015-2.sql
@@ -1,4 +1,7 @@
-select ename, hiredate
+alter session set nls_date_format = 'RR-MM-DD';
+alter session set nls_date_language = 'JAPANESE';
+
+select ename, hiredate, deptno
 from employees
 where deptno = 10
 /
