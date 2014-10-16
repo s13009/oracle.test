ab80d139e4fa55d55d59a982881b9e4e2610a86d
s13009 <s13009@std.it-college.ac.jp>
2014-10-16 15:01:22 +0900

    修正

--- 141016-3.sql
+++ 141016-3.sql
@@ -1,6 +1,3 @@
-alter session set nls_date_format = 'DD-MON-RR';
-alter session set nls_date_language = 'AMERICAN';
-
-select ename, TO_CHAR(hiredate, 'DDTH "of" MONTH')
+select ename, TO_CHAR(hiredate, 'DDTH "of" Month')
 from employees
 /
