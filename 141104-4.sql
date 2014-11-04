577ca581226a7cf4940cb2d8ff02e8492b514667
s13009 <s13009@std.it-college.ac.jp>
2014-11-04 15:43:09 +0900

    11/04

--- 141104-4.sql
+++ 141104-4.sql
@@ -1,10 +1,11 @@
-create table dept_copy as select* from departments where 0 = 1
-/
-
 INSERT INTO dept_copy
 SELECT deptno+1, dname, loc
-FROM departments;
+FROM departments
+/
 
 select *
 from dept_copy
+/
+
+rollback
 /
